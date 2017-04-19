2a0b1d35712aebcee40ce8ef309eb2483eec66c3
ashraf.akl <ashrafakl@yahoo.com>
2017-04-19 23:38:49 +0200

    Locale class represents the data relevant to a locale such as countries, languages, and orientation

---
+++ README.md
@@ -0,0 +1,4 @@
+# yii2-locale
+Locale class represents the data relevant to a locale such as countries, languages, and orientation.
+
+This class uses and depend on the PHP intl extension.
---
+++ src/Locale.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * @package   yii2-jsTree
+ * @author    Ashraf Akl <ashrafakl@yahoo.com>
+ * @copyright Copyright &copy; Ashraf Akl, 2014 - 2017
+ * @version   1.0.0
+ */
+
+namespace ashrafakl;
+
+use yii\base\Component;
+use Yii;
+
+/**
+ * Locale class represents the data relevant to a locale such as countries, languages, and orientation, 
+ * this class uses and depend on the PHP intl extension.
+ * @author ashrafakl@yahoo.com
+ */
+class Locale extends Component
+{
+
+    private $locales;
+    private $countries;
+    private $languages;
+    private $rtlLanguages = ['ar', 'dv', 'fa', 'he', 'ks', 'ku', 'ps', 'syr', 'ur'];
+
+    /**
+     * Path of ResourceBundle for which to get available locales, or empty string for default locales list.
+     * @see http://php.net/manual/en/resourcebundle.locales.php
+     * @var string 
+     */
+    public $bundlename = '';
+
+    /**
+     * Initializes Local.
+     */
+    public function init()
+    {        
+        if ($this->locales === null) {
+            $this->locales = \ResourceBundle::getLocales($this->bundlename);
+        }
+        parent::init();
+    }
+
+    /**
+     * Get Countries list
+     * @param string $inLocale format locale to use to display the regions names, default to current yii language     
+     * @return array      
+     */
+    public function getCountries($inLocale = null)
+    {
+        if (!$inLocale) {
+            $inLocale = Yii::$app->language;
+        }
+        $key = strtolower($inLocale);
+        if (!isset($this->countries[$key])) {
+            foreach ($this->locales as $locale) {
+                $countryName = \Locale::getDisplayRegion($locale, $inLocale);
+                if ($countryName) {
+                    $territoryId = \Locale::getRegion($locale);
+                    if (!is_numeric($territoryId)) {
+                        $this->countries[$key][strtolower($territoryId)] = $countryName;
+                    }
+                }
+            }
+        }
+        return $this->countries[$key];
+    }
+
+    /**
+     * Get languages list
+     * @param string $inLocale format locale to use to display the languages names, default to current yii language     
+     * @return array      
+     */
+    public function getPrimaryLanguages($inLocale = null)
+    {
+        if (!$inLocale) {
+            $inLocale = Yii::$app->language;
+        }
+        $key = strtolower($inLocale);
+        if (!isset($this->languages[$key])) {
+            foreach ($this->locales as $locale) {
+                $this->languages[$key][\Locale::getPrimaryLanguage($locale)] = \Locale::getDisplayLanguage($locale, $inLocale);
+            }
+        }
+        return $this->languages[$key];
+    }
+
+    /**
+     * Get document orientation, which is either 'ltr' (left-to-right) or 'rtl' (right-to-left)
+     * @param string $inLocale format locale to use to gets the orientation, default to current yii language     
+     * @return string
+     */
+    public function getOrientation($inLocale = null)
+    {
+        if (!$inLocale) {
+            $inLocale = Yii::$app->language;
+        }        
+        return in_array(\Locale::getPrimaryLanguage($inLocale), $this->rtlLanguages) ? 'rtl' : 'ltr';
+    }
+}
